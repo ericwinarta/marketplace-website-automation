de452660ece5eb54c1deb8f4de20ec8e6dd97b88
ericwinarta <eric_winarta@hotmail.com>
2024-06-07 20:37:18 +0700

    add retry login attempt

--- src/main/java/marketplace/pageObject/LoginPage.java
+++ src/main/java/marketplace/pageObject/LoginPage.java
@@ -22,6 +22,7 @@ public class LoginPage extends AbstractComponent {
 	
 	By emailInputLocator = By.xpath("//div[text()='Email']/parent::div//input");
 	By passwordInputLocator = By.xpath("//input[@type='password']");
+	By dangerWarningMessageLocator = By.cssSelector("div[color='danger'] span");
 	
 	@FindBy(xpath = "//span[text()='Selanjutnya']/parent::button")
 	WebElement nextButton;
@@ -29,11 +30,81 @@ public class LoginPage extends AbstractComponent {
 	public void loginApplication(UserData user) {
 		WebElement emailInput = waitForElementToAppear(emailInputLocator);
 		emailInput.sendKeys(user.getEmail());
-		nextButton.sendKeys(Keys.ENTER);
 		
-		WebElement passwordInput = waitForElementToAppear(passwordInputLocator);
+		int inputEmailAttempt = 0;
+		int inputEmailMaxAttempt = 5;
+		
+		WebElement passwordInput = null;
+		
+		while (inputEmailAttempt < inputEmailMaxAttempt) {
+			try {
+				nextButton.sendKeys(Keys.ENTER);
+				passwordInput = waitForElementToAppear(passwordInputLocator);
+				break;
+			} catch (Exception e) {
+				inputEmailAttempt++;
+				
+				if (inputEmailAttempt >= inputEmailMaxAttempt) {
+					throw new RuntimeException("Next button failed for " + inputEmailMaxAttempt  + " clicked attempt, after type email address");
+				}
+			}
+		}
+		
 		passwordInput.sendKeys(user.getPassword());
 		nextButton.click();
+	}
+	
+	public String verifyInvalidEmail(String email) {
+		WebElement emailInput = waitForElementToAppear(emailInputLocator);
+		emailInput.sendKeys(email);
+		
+		int warningBoxAttempt = 0;
+		int warningBoxMaxAttempt = 3;
+		
+		while (warningBoxAttempt < warningBoxMaxAttempt) {
+			try {
+				nextButton.sendKeys(Keys.ENTER);
+				WebElement warningMessage = waitForElementToAppear(dangerWarningMessageLocator);
+				return warningMessage.getText();
+			} catch (Exception e) {
+				warningBoxAttempt++;
+				
+				if (warningBoxAttempt >= warningBoxMaxAttempt) {
+					break;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	public String verifyInvalidPassword(String email, String password) {
+		WebElement emailInput = waitForElementToAppear(emailInputLocator);
+		emailInput.sendKeys(email);
+		
+		int inputEmailAttempt = 0;
+		int inputEmailMaxAttempt = 3;
+		
+		WebElement passwordInput = null;
+		
+		while (inputEmailAttempt < inputEmailMaxAttempt) {
+			try {
+				nextButton.sendKeys(Keys.ENTER);
+				passwordInput = waitForElementToAppear(passwordInputLocator);
+				break;
+			} catch (Exception e) {
+				inputEmailAttempt++;
+				
+				if (inputEmailAttempt >= inputEmailMaxAttempt) {
+					break;
+				}
+			}
+		}
+		
+		passwordInput.sendKeys(password);
+		nextButton.click();
+		WebElement warningMessage = waitForElementToAppear(dangerWarningMessageLocator);
+		return warningMessage.getText();
 	}
 
 }
