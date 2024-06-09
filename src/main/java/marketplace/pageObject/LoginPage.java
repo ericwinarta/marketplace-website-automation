6d0e66f8b63afdccdb5afcfb09d6bdb00de14f42
ericwinarta <eric_winarta@hotmail.com>
2024-06-09 17:38:25 +0700

    refactoring code

--- src/main/java/marketplace/pageObject/LoginPage.java
+++ src/main/java/marketplace/pageObject/LoginPage.java
@@ -23,32 +23,19 @@ public class LoginPage extends AbstractComponent {
 	By emailInputLocator = By.xpath("//div[text()='Email']/parent::div//input");
 	By passwordInputLocator = By.xpath("//input[@type='password']");
 	By dangerWarningMessageLocator = By.cssSelector("div[color='danger'] span");
+	By registerLinkLocator = By.cssSelector("a[href='/register']");
 	
 	@FindBy(xpath = "//span[text()='Selanjutnya']/parent::button")
 	WebElement nextButton;
+	
+	@FindBy(css = "a[href='/register']")
+	WebElement registerLink;
 	
 	public void loginApplication(UserData user) {
 		WebElement emailInput = waitForElementToAppear(emailInputLocator);
 		emailInput.sendKeys(user.getEmail());
 		
-		int inputEmailAttempt = 0;
-		int inputEmailMaxAttempt = 5;
-		
-		WebElement passwordInput = null;
-		
-		while (inputEmailAttempt < inputEmailMaxAttempt) {
-			try {
-				nextButton.sendKeys(Keys.ENTER);
-				passwordInput = waitForElementToAppear(passwordInputLocator);
-				break;
-			} catch (Exception e) {
-				inputEmailAttempt++;
-				
-				if (inputEmailAttempt >= inputEmailMaxAttempt) {
-					throw new RuntimeException("Next button failed for " + inputEmailMaxAttempt  + " clicked attempt, after type email address");
-				}
-			}
-		}
+		WebElement passwordInput = inputEmailNextButtonAttempt(emailInput, passwordInputLocator);
 		
 		passwordInput.sendKeys(user.getPassword());
 		nextButton.click();
@@ -58,53 +45,50 @@ public class LoginPage extends AbstractComponent {
 		WebElement emailInput = waitForElementToAppear(emailInputLocator);
 		emailInput.sendKeys(email);
 		
-		int warningBoxAttempt = 0;
-		int warningBoxMaxAttempt = 3;
+		WebElement warningMessage = inputEmailNextButtonAttempt(emailInput, dangerWarningMessageLocator);
 		
-		while (warningBoxAttempt < warningBoxMaxAttempt) {
-			try {
-				nextButton.sendKeys(Keys.ENTER);
-				WebElement warningMessage = waitForElementToAppear(dangerWarningMessageLocator);
-				return warningMessage.getText();
-			} catch (Exception e) {
-				warningBoxAttempt++;
-				
-				if (warningBoxAttempt >= warningBoxMaxAttempt) {
-					break;
-				}
-			}
-		}
-		
-		return null;
+		return warningMessage.getText();
 	}
 	
 	public String verifyInvalidPassword(String email, String password) {
 		WebElement emailInput = waitForElementToAppear(emailInputLocator);
 		emailInput.sendKeys(email);
 		
-		int inputEmailAttempt = 0;
-		int inputEmailMaxAttempt = 3;
-		
-		WebElement passwordInput = null;
-		
-		while (inputEmailAttempt < inputEmailMaxAttempt) {
-			try {
-				nextButton.sendKeys(Keys.ENTER);
-				passwordInput = waitForElementToAppear(passwordInputLocator);
-				break;
-			} catch (Exception e) {
-				inputEmailAttempt++;
-				
-				if (inputEmailAttempt >= inputEmailMaxAttempt) {
-					break;
-				}
-			}
-		}
+		WebElement passwordInput = inputEmailNextButtonAttempt(emailInput, passwordInputLocator);
 		
 		passwordInput.sendKeys(password);
 		nextButton.click();
 		WebElement warningMessage = waitForElementToAppear(dangerWarningMessageLocator);
 		return warningMessage.getText();
+	}
+	
+	public WebElement inputEmailNextButtonAttempt(WebElement input, By locatorToWait) {
+		int inputEmailAttempt = 0;
+		int inputEmailMaxAttempt = 5;
+		
+		WebElement elementToWait = null;
+		
+		while (inputEmailAttempt < inputEmailMaxAttempt) {
+			try {
+				input.sendKeys(Keys.ENTER);
+				elementToWait = waitForElementToAppear(locatorToWait);
+				break;
+			} catch (Exception e) {
+				inputEmailAttempt++;
+				
+				if (inputEmailAttempt >= inputEmailMaxAttempt) {
+					throw new RuntimeException("Next button failed for " + inputEmailMaxAttempt  + " clicked attempt, after type email address");
+				}
+			}
+		}
+		return elementToWait;
+	}
+	
+	public RegisterPage goToRegister() {
+		waitForElementToAppear(registerLinkLocator);
+		registerLink.click();
+		RegisterPage registerPage = new RegisterPage(driver);
+		return registerPage;
 	}
 
 }
