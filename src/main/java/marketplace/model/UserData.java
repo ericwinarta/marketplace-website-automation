bcbeb37161b28d64f0697aed2a1050e1ac67d8a2
ericwinarta <eric_winarta@hotmail.com>
2024-06-09 17:38:07 +0700

    Update UserData.java

--- src/main/java/marketplace/model/UserData.java
+++ src/main/java/marketplace/model/UserData.java
@@ -6,6 +6,9 @@ public class UserData {
 	
 	private String email;
 	private String password;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber; 
 	private List<Product> product;
 	private Address address;
 	private String shippingDelivery;
@@ -24,6 +27,30 @@ public class UserData {
 	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 	
 	public List<Product> getProduct() {
